baabec44d98e49ec7059dae34bd67843c1b89cd0
gunho <rkdus5964@gmail.com>
2022-05-22 14:29:03 +0900

    12 commit

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -85,3 +85,79 @@ const obj: Dropdown<Number> = {
 };
 
 // 인터페이스에 제네릭을 활용하므로 안의 속성의 타입을 얼마든지 바꿀 수 있음
+
+
+
+// 제네릭의 타입 제한
+
+// function logTextLength<T>(text: T): T {
+//  console.log(text.length);
+
+// 위 코드에서 text.length 가 있다는건 개발자 입장에서 알 수 있음.
+// 타입 스크립트 입장에서 배열이 올거라는걸 명시적으로 알려줘야함.
+
+// function logTextLength<T>(text: T[]): T[] {
+//  console.log(text.length);
+
+// T[] 을 넣어줌으로써 배열이 들어올 것이라고 알려줘야
+// 타입스크립트에서 배열에 관한 api, 속성을 지원해줌
+// 따라서 마지막 코드에도 그냥 문자열이 아닌 배열로 전달을 해줘야호출이 가능함
+
+// logTextLength<string>(["hi", "abc"]);
+ 
+// function logTextLength<T>(text: T[]): T[] {
+//   console.log(text.length);
+//   text.forEach(function (text) {
+//     console.log(text);
+//   });
+//   return text;
+// }
+// logTextLength<string>(['hi', "abc"])
+
+// 제네릭 타입 제한 2 - 정의된 타입 이용하기
+// 이미 정의된 타입을 확장해서 쓸 때 extends 를 쓴다.
+interface LengthType {
+  length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T {
+  text.length;
+  return text;
+}
+// logTextLength(10);
+logTextLength('a');
+logTextLength(['a', 10]);
+
+// 10은 length 프로퍼티가 제공되지 않기때문에 에러가 난다.
+// 문자열과 배열은 length 가 제공이 됨.
+// 인자 타입에 선언한 T 는 아직 어떤 타입인지 구분하지 않았기때문에 length에서 오류가 난다.
+//  interface LengthType {
+//   length: number;
+// } 
+
+// 타입을 정의하지 않고도 length를 쓰려면 위와 같이 작성한다.
+// 위처럼 작성하게 되면 강제는 아니지만 length 에 동작하는 인자만 넘겨받을 수 있게된다.
+
+
+
+// 제네릭의 타입 제한 3 - keyof
+
+// 여기서 getShoopingItemOption 함수에 
+// interface getShoopingItem 에 정의된 속섬만 받게하고 싶다. 제한 할 수 있음
+
+// 기존에 정의되어있는 인터페이스, 타입을 확장할때 쓰는게 extends + ket of 
+// extends keyof ShoppingItem = shoppingitem 에 있는 키들 중에 하나가 <T>제네릭이다.
+interface ShoppingItem {
+  name: string;
+  price: number;
+  stock: number;
+}
+
+function getShoopingItemOption<T extends keyof ShoppingItem >(itemOption: T): T {
+    return itemOption;
+}
+
+// getShoopingItemOption(10);
+// getShoopingItemOption<string>("a");
+getShoopingItemOption('name')
+// keyof = 이미 정의되어있는 키값들만 인자로 받을 수 있다 라고 제한하는거임
+// 
