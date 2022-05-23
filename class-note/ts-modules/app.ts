6da2308f66f511fe79324cb73feb5eded388931f
gunho <rkdus5964@gmail.com>
2022-05-23 14:51:55 +0900

    commit

---
+++ class-note/ts-modules/app.ts
@@ -0,0 +1,20 @@
+import { Todo } from "./types"    
+// types에 있는 
+// interface Todo {
+//   title: string;
+//   checked: boolean;
+// } Todo 가져온거임
+
+
+
+
+// item 이라는 변수에  Todo 속성을 정의한거임 
+// Todo 엔 title:stirng, checked:boolean; 이 있음
+var item: Todo = {
+    title: "할일 1",
+    checked: false
+}
+
+
+// 모듈 시스템
+// 다른 파일에서 정의해놓은 변수나 함수를 들고와서 활용하는거임
