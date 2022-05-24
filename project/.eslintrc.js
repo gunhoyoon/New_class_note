1abfe3191b8ee9f06355cbb8777a37bf2d267bfc
gunho <rkdus5964@gmail.com>
2022-05-24 14:37:59 +0900

    type2 commit4

--- project/.eslintrc.js
+++ project/.eslintrc.js
@@ -6,14 +6,16 @@ module.exports = {
     node: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
   ],
-  plugins: ["prettier", "@typescript-eslint"],
+  plugins: ['prettier', '@typescript-eslint'],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      // prettier에 정의된 규칙대로 안하면 전부 에러로 처리하겠다
+      // 워닝정도로 바꿀 수도 있음 밑에 warn 으로 바꾸면 됨
+      'error',
       {
         singleQuote: true,
         semi: true,
@@ -21,11 +23,11 @@ module.exports = {
         tabWidth: 2,
         printWidth: 80,
         bracketSpacing: true,
-        arrowParens: "avoid",
+        arrowParens: 'avoid',
       },
     ],
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: '@typescript-eslint/parser',
   },
 };
